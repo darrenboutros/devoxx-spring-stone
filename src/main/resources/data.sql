9694263b6eb932a827661c5cce4d8d9f29082eaf
Darren Boutros <darren.boutros@gmail.com>
2019-04-14 19:08:26 +0200

    load database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO stone VALUES (1, 'Time' , 'Green');
+INSERT INTO stone VALUES (2, 'Power', 'Purple');
+INSERT INTO stone VALUES (3, 'Space', 'Blue');
+INSERT INTO stone VALUES (4, 'Reality', 'Red');
+INSERT INTO stone VALUES (5, 'Soul', 'Orange');
+INSERT INTO stone VALUES (6, 'Mind', 'Yellow');
