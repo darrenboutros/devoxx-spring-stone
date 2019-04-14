6119e2eac3b2bb43a3419193fb5f2e1fee7f74c4
Darren Boutros <darren.boutros@gmail.com>
2019-04-14 19:07:34 +0200

    annotation @Entity, @Table, @Id, @GeneratedValue, @NotNull

--- src/main/java/fr/devoxx/spring/stone/model/Stone.java
+++ src/main/java/fr/devoxx/spring/stone/model/Stone.java
@@ -1,13 +1,25 @@
 package fr.devoxx.spring.stone.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table
 @Data @NoArgsConstructor @AllArgsConstructor
 public class Stone {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
+	@NotNull
 	private String name;
+	@NotNull
 	private String color;
 }
